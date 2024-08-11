74fbd011f04f8ccfe2090cd125e171dfa2a45d03
Catrina_Evilsizer <tranqcalypso@gmail.com>
2024-08-11 09:37:56 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
+import fs from "fs";
+import prompt from "inquirer/lib/ui/prompt";
 
 // TODO: Create an array of questions for user input
 const questions = [
+    prompt([
     {
         type: "input",
         name:"title",
         message: "What is the title of your project?",
     },
     {
-        type:"input"
+        type:"input",
         name:"description",
-        message: "Please give a brief description of your project."
+        message: "Please give a brief description of your project.",
     },
     {
         type: "input",
         name: "installation",
-        message: "What are the installation instructions for your project?"
+        message: "What are the installation instructions for your project?",
     },
     {
         type: "input",
         name: "usage",
-        message: "What is the usage information for your project?"
+        message: "What is the usage information for your project?",
     },
     { 
         type: "input",
-        name: "contributors"
-        message: "Who are the contributors to your project?"
+        name: "contributors",
+        message: "Who are the contributors to your project?",
     },
     {
         type: "list",
@@ -34,16 +37,19 @@ const questions = [
         message: "What license would you like to use?",
         choices: ["None","Apache License 2.0","GNU General Public License v3.0","MIT License 2.0","BSD 2 - Clause 'Simplified' License","BSD 3 Clause 'New' or 'Revised' License","Boost Software License","Creative Commons Zero v1.0 Universal","Eclipse Public License 2.0","GNU General Public License ","GNU Affero General Public License v3.0","GNU Lesser General Public License v2.1","Mozilla Public License 2.0","The Unlicense"],
     }
-];
+],
 // TODO: Create a function to write README file
-function writeToFile("README.md", data) {
-    
-
-}
+const writeToLog = (data) => {
+  fs.writeFile('README.md', data, (err) => {
+    err ? console.error(err) : console.log('Log created!');
+  });
+};
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
